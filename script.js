d38c129fd3930dd813d2faffedd27cd7ff84b895
tjdwlgns612 <jsung612@gmail.com>
2022-03-26 14:17:20 -0400

    done!

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // CLICKING ON RATINGS
-
 var activeBtn = "";
 
-document.getElementById("btn__1").onclick = function(){btnPressed("1")};
-document.getElementById("btn__2").onclick = function(){btnPressed("2")};
-document.getElementById("btn__3").onclick = function(){btnPressed("3")};
-document.getElementById("btn__4").onclick = function(){btnPressed("4")};
-document.getElementById("btn__5").onclick = function(){btnPressed("5")};
+const btn1 = document.getElementById("btn__1");
+const btn2 = document.getElementById("btn__2");
+const btn3 = document.getElementById("btn__3");
+const btn4 = document.getElementById("btn__4");
+const btn5 = document.getElementById("btn__5");
+
+btn1.onclick = function(){btnPressed("1")};
+btn2.onclick = function(){btnPressed("2")};
+btn3.onclick = function(){btnPressed("3")};
+btn4.onclick = function(){btnPressed("4")};
+btn5.onclick = function(){btnPressed("5")};
 
 function btnPressed(num_string) {
     if (activeBtn == "") {
@@ -32,4 +37,20 @@ function btnPressed(num_string) {
     }
 
     activeBtn = num_string;
+}
+
+// CLICKING SUBMIT
+const btnSubmit = document.getElementById("btn__submit");
+const container = document.getElementById("container");
+const containerSubmitted = document.getElementById("container__submitted");
+const selectedRating = document.getElementById("selected__rating");
+
+btnSubmit.onclick = function(){submitted(activeBtn)};
+
+function submitted(rating) {
+    if (activeBtn != "") {
+        selectedRating.textContent = rating;
+        container.style.display = "none";
+        containerSubmitted.style.display = "block";
+    }
 }
